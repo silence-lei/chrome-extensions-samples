c4320f4b2f9950a79ab6fc4a3d434b6de8670cbe
leimengfan <1665584353@qq.com>
2024-05-15 17:20:28 +0800

    browsingData代码格式优化

--- api-samples/browsingData/popup.js
+++ api-samples/browsingData/popup.js
@@ -13,64 +13,64 @@
 // limitations under the License.
 
 function parseMilliseconds(timeframe) {
-  let now = new Date().getTime();
-  let milliseconds = {
-    hour: 60 * 60 * 1000,
-    day: 24 * 60 * 60 * 1000,
-    week: 7 * 24 * 60 * 60 * 1000,
-    '4weeks': 4 * 7 * 24 * 60 * 60 * 1000
-  };
+    let now = new Date().getTime();
+    let milliseconds = {
+        hour: 60 * 60 * 1000,
+        day: 24 * 60 * 60 * 1000,
+        week: 7 * 24 * 60 * 60 * 1000,
+        '4weeks': 4 * 7 * 24 * 60 * 60 * 1000
+    };
 
-  if (milliseconds[timeframe]) return now - milliseconds[timeframe];
+    if (milliseconds[timeframe]) return now - milliseconds[timeframe];
 
-  if (timeframe === 'forever') return 0;
+    if (timeframe === 'forever') return 0;
 
-  return null;
+    return null;
 }
 
 function buttonClicked() {
-  const option = document.getElementById('timeframe');
-  let selectedTimeframe = option.value;
-  let removal_start = parseMilliseconds(selectedTimeframe);
-  if (removal_start == undefined) {
-    return null;
-  }
-  chrome.browsingData.remove(
-    { since: removal_start },
-    {
-      appcache: true,
-      cache: true,
-      cacheStorage: true,
-      cookies: true,
-      downloads: true,
-      fileSystems: true,
-      formData: true,
-      history: true,
-      indexedDB: true,
-      localStorage: true,
-      serverBoundCertificates: true,
-      serviceWorkers: true,
-      pluginData: true,
-      passwords: true,
-      webSQL: true
+    const option = document.getElementById('timeframe');
+    let selectedTimeframe = option.value;
+    let removal_start = parseMilliseconds(selectedTimeframe);
+    if (removal_start == undefined) {
+        return null;
     }
-  );
-  const success = document.createElement('div');
-  success.classList.add('overlay');
-  success.setAttribute('role', 'alert');
-  success.textContent = 'Data has been cleared.';
-  document.body.appendChild(success);
+    chrome.browsingData.remove(
+        { since: removal_start },
+        {
+            appcache: true,
+            cache: true,
+            cacheStorage: true,
+            cookies: true,
+            downloads: true,
+            fileSystems: true,
+            formData: true,
+            history: true,
+            indexedDB: true,
+            localStorage: true,
+            serverBoundCertificates: true,
+            serviceWorkers: true,
+            pluginData: true,
+            passwords: true,
+            webSQL: true
+        }
+    );
+    const success = document.createElement('div');
+    success.classList.add('overlay');
+    success.setAttribute('role', 'alert');
+    success.textContent = 'Data has been cleared.';
+    document.body.appendChild(success);
 
-  setTimeout(function () {
-    success.classList.add('visible');
-  }, 10);
-  setTimeout(function () {
-    if (close === false) success.classList.remove('visible');
-    else window.close();
-  }, 4000);
+    setTimeout(function () {
+        success.classList.add('visible');
+    }, 10);
+    setTimeout(function () {
+        if (close === false) success.classList.remove('visible');
+        else window.close();
+    }, 4000);
 }
 
 window.addEventListener('DOMContentLoaded', function () {
-  const selection = document.getElementById('button');
-  selection.addEventListener('click', buttonClicked);
+    const selection = document.getElementById('button');
+    selection.addEventListener('click', buttonClicked);
 });
