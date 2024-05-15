a805784fd5beb918e45074ba54c9424c9b21e9d8
leimengfan <1665584353@qq.com>
2024-05-15 16:30:01 +0800

    alarms代码格式优化

--- api-samples/alarms/background.js
+++ api-samples/alarms/background.js
@@ -6,21 +6,21 @@
 
 // Initialize the demo on install
 chrome.runtime.onInstalled.addListener(({ reason }) => {
-  if (reason !== chrome.runtime.OnInstalledReason.INSTALL) {
-    return;
-  }
+    if (reason !== chrome.runtime.OnInstalledReason.INSTALL) {
+        return;
+    }
 
-  openDemoTab();
+    openDemoTab();
 
-  // Create an alarm so we have something to look at in the demo
-  chrome.alarms.create('demo-default-alarm', {
-    delayInMinutes: 1,
-    periodInMinutes: 1
-  });
+    // Create an alarm so we have something to look at in the demo
+    chrome.alarms.create('demo-default-alarm', {
+        delayInMinutes: 1,
+        periodInMinutes: 1
+    });
 });
 
 chrome.action.onClicked.addListener(openDemoTab);
 
 function openDemoTab() {
-  chrome.tabs.create({ url: 'index.html' });
+    chrome.tabs.create({ url: 'index.html' });
 }
